a682fabeebd66a8e9cfeba39348599b44ee270a3
= <jloscalzo@hxlws-jloscalzo.hexacta.com>
2015-07-13 13:17:43 -0300

    TDD Romanos. Terminado

--- TDDHexacta/Roman.cs
+++ TDDHexacta/Roman.cs
@@ -1,18 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
 
 namespace TDDHexacta
 {
-    class Roman
+    internal class Roman
     {
-        static int[] nums = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 2, 1 };
-        static string[] rum = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "II","I" };
-
-        public static string ToRoman(int number)
+        private static readonly Dictionary<int, string> numIntToRoman = new Dictionary<int, string>()
         {
-            var index  = nums.ToList().IndexOf(number);
-            return rum[index];
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1,"I"}
+
+        };
+
+        public Roman()
+        {
+
+        }
+
+
+
+        public string ToRoman(int number)
+        {
+            if (number >= 3888)
+            {
+                throw new ArgumentOutOfRangeException("number", message: "Numeros mayores que 3888 no se pueden fabricar");
+            }
+            
+            foreach (var t in numIntToRoman)
+            {
+                if (number >= t.Key)
+                {
+                    var val = number - t.Key;
+                    return string.Concat(t.Value, new Roman().ToRoman(val));
+                }
+            }
+            return null;
         }
     }
 }
--- TDDHexacta/RomanTest.cs
+++ TDDHexacta/RomanTest.cs
@@ -1,13 +1,40 @@
 ﻿using System;
+using System.Collections;
 using System.Runtime.Remoting.Messaging;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace TDDHexacta
 {
-    [TestClass]
+    [TestFixture]
     public class RomanTest
     {
-        [TestMethod]
+        /*
+Como regla general, los símbolos se escriben y leen de izquierda a derecha, de mayor a menor valor.
+El valor de un número se obtiene sumando los valores de los símbolos que lo componen, salvo en la siguiente excepción.
+Si un símbolo de tipo 1 está a la izquierda inmediata de otro de mayor valor, se resta al valor del segundo el valor del primero. Ej. IV=4, IX=9.
+Los símbolos de tipo 5 siempre suman y no pueden estar a la izquierda de uno de mayor valor.
+Se permiten a lo sumo tres repeticiones consecutivas del mismo símbolo de tipo 1.
+No se permite la repetición de una misma letra de tipo 5, su duplicado es una letra de tipo 10.
+Si un símbolo de tipo 1 aparece restando, sólo puede aparecer a su derecha un sólo símbolo de mayor valor.
+Si un símbolo de tipo 1 que aparece restando se repite, sólo se permite que su repetición esté colocada a su derecha y que no sea adyacente al símbolo que resta.
+Sólo se admite la resta de un símbolo de tipo 1 sobre el inmediato mayor de tipo 1 o de tipo 5. Ejemplos:
+el símbolo I sólo puede restar a V y a X.
+el símbolo X sólo resta a L y a C.
+el símbolo C sólo resta a D y a M.
+Se permite que dos símbolos distintos aparezcan restando si no son adyacentes.
+         */
+
+        private Roman Roman;
+
+        [SetUp]
+        public void CreateRoman()
+        {
+            this.Roman = new Roman();
+        }
+
+
+        [Test]
         public void TestDigitos()
         {
             Assert.AreEqual(Roman.ToRoman(1), "I");
@@ -20,25 +47,19 @@ namespace TDDHexacta
 
         }
 
-        [TestMethod, Ignore]
-        public void TestSumaADerecha()
-        {
-
-        }
-
-        [TestMethod, Ignore]
+        [Test]
         public void TestRestaAIzquierda()
         {
             Assert.AreEqual("IV", Roman.ToRoman(4));
             Assert.AreEqual("XL", Roman.ToRoman(40));
-            Assert.AreEqual("CM", Roman.ToRoman(400));
+            Assert.AreEqual("CD", Roman.ToRoman(400));
 
             Assert.AreEqual("IX", Roman.ToRoman(9));
             Assert.AreEqual("XC", Roman.ToRoman(90));
             Assert.AreEqual("CM", Roman.ToRoman(900));
         }
 
-        [TestMethod, Ignore]
+        [Test]
         public void TestMenos3Repeticiones()
         {
             Assert.AreEqual("III", Roman.ToRoman(3));
@@ -48,15 +69,35 @@ namespace TDDHexacta
         }
 
 
-        [TestMethod, Ignore]
+        [Test]
         public void TestMenos2Repeticiones()
         {
-            Assert.AreEqual("III", Roman.ToRoman(3));
-            Assert.AreEqual("XXX", Roman.ToRoman(30));
-            Assert.AreEqual("CCC", Roman.ToRoman(300));
-            Assert.AreEqual("MMM", Roman.ToRoman(3000));
+            Assert.AreEqual("II", Roman.ToRoman(2));
+            Assert.AreEqual("XX", Roman.ToRoman(20));
+            Assert.AreEqual("CC", Roman.ToRoman(200));
+            Assert.AreEqual("MM", Roman.ToRoman(2000));
         }
 
+        [TestCaseSource(typeof(RomanTestData), "EspecialCases")]
+        public string EspecialCasesTest(int value)
+        {
+            return Roman.ToRoman(value);
+        }
 
     }
+
+    public class RomanTestData
+    {
+        public static IEnumerable EspecialCases
+        {
+            get
+            {
+                yield return new TestCaseData(1).Returns("I");
+                yield return new TestCaseData(1492).Returns("MCDXCII");
+                yield return new TestCaseData(3889).Throws(typeof(ArgumentOutOfRangeException));
+            }
+        }
+    }
+
+
 }
